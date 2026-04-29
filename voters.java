30466314a926016dfa68015cc29aed5eaef64de3
RAVIRAGUL-174 <saranragul25@gmail.com>
2026-04-29 10:22:40 +0530

    program
    Signed-off-by: RAVIRAGUL-174 <saranragul25@gmail.com>

---
+++ voters.java
@@ -0,0 +1,11 @@
+class voters {
+    public static void main(String[] args) {
+        int age=4;
+        if (age >= 18) {
+            System.out.println("ELIGIBLE");
+        }
+        else {
+            System.out.println("NOT ELIGIBLE");
+        }
+}
+}
