b0572fe3798a90f0dbff417db34076f1afac6b60
RAVIRAGUL-174 <saranragul25@gmail.com>
2026-04-30 09:47:36 +0530

    program
    Signed-off-by: RAVIRAGUL-174 <saranragul25@gmail.com>

---
+++ grade.java
@@ -0,0 +1,13 @@
+
+class grade {
+    public static void main(String[] args) {
+        int score=30;
+        if (score >= 40) {
+            System.out.println("Pass");
+        } 
+         else {
+            System.out.println("Fail");
+        }
+    }
+}
+
