43d31d15bd3f964ffd5e09e6f59a0262a0933165
RAVIRAGUL-174 <saranragul25@gmail.com>
2026-05-01 10:06:33 +0530

    program
    Signed-off-by: RAVIRAGUL-174 <saranragul25@gmail.com>

---
+++ bio.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class bio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Enter your ph.no.:");
+        long phoneNumber = sc.nextLong(); 
+        System.out.println("Enter email:");
+        String email = sc.next();
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(phoneNumber);
+        System.out.println(email);
+    
+    }
+}
